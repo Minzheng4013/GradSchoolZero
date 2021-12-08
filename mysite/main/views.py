48a6da9a3fa2e31c8e5fe8182917f6eb08c6eb0b
Minzheng4013 <mimizheng4013@gmail.com>
2021-12-07 21:19:17 -0500

    update

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,21 +3,22 @@ from account.models import *
 
 # Create your views here.
 def home(request):
-	students = Student.objects.all()
-	courses = Course.objects.all()
-	max_rate = 0
-	max_gpa = 0
+	# students = Student.objects.all()
+	# courses = Course.objects.all()
+	# max_rate = 0
+	# max_gpa = 0
 
-	for student in students:
-		if student.GPA > max_gpa:
-			max_gpa = student.GPA
-	top_students = Student.objects.filter(GPA=max_gpa)
+	# for student in students:
+	# 	if student.GPA > max_gpa:
+	# 		max_gpa = student.GPA
+	# top_students = Student.objects.filter(GPA=max_gpa)
 
-	for course in courses:
-		if course.rate > max_rate:
-			max_rate = course.rate
-	top_classes = Course.objects.filter(rate=max_rate)
+	# for course in courses:
+	# 	if course.rate > max_rate:
+	# 		max_rate = course.rate
+	# top_classes = Course.objects.filter(rate=max_rate)
 
-	return render(request, "main/home.html", {'ts': top_students, 'tc': top_classes, 'c':courses})
+	# return render(request, "main/home.html", {'ts': top_students, 'tc': top_classes, 'c':courses})
+	return render(request, "main/home.html")
 
 
